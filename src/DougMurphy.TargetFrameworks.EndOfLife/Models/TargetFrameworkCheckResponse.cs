eb2fce6c8d654cabadda4941c95d1dfdf115eae4
Doug Murphy <doug.murphy82@gmail.com>
2021-12-02 16:46:13 -0500

    Add XML comments on public facing members that were previously missing them.

--- src/DougMurphy.TargetFrameworks.EndOfLife/Models/TargetFrameworkCheckResponse.cs
+++ src/DougMurphy.TargetFrameworks.EndOfLife/Models/TargetFrameworkCheckResponse.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections.Generic;
 
 namespace DougMurphy.TargetFrameworks.EndOfLife.Models {
+	/// <summary>
+	/// Holds the response from the EOL check to.
+	/// </summary>
+	/// <param name="EndOfLifeTargetFrameworks">The list of TFMs that were determined to be EOL.</param>
 	public record TargetFrameworkCheckResponse(IReadOnlyList<string> EndOfLifeTargetFrameworks) {
+		/// <summary>
+		/// The list of TFMs that were determined to be EOL.
+		/// </summary>
 		public IReadOnlyList<string> EndOfLifeTargetFrameworks { get; } = EndOfLifeTargetFrameworks;
 	}
 }
